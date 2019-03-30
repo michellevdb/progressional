9f2994d70e774ad3c199be51ee12a6ed349ac701
Ritchie <ritch.g@gmail.com>
2019-03-30 09:26:04 -0700

    move carmens schema file to db folder

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS fitnessDB;
+
+CREATE DATABASE fitnessDB;
+
+USE fitnessDB;
--- models/schema.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS fitnessDB;
-
-CREATE DATABASE fitnessDB;
-
-USE fitnessDB;
