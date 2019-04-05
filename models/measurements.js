a7bfab866370853304df93492d40484a1eea51df
Ritchie <detectives.salvajes@protonmail.com>
2019-04-05 08:41:14 -0700

    adds validations to measure input. needs handling

--- models/measurements.js
+++ models/measurements.js
@@ -9,51 +9,98 @@ module.exports = function (sequelize, DataType) {
     weight: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     chest: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     waist: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     beltLine: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     leftBicep: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     rightBicep: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     leftForearm: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     rightForearm: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     leftThigh: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     rightThigh: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     leftCalf: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     rightCalf: {
       type: DataType.DECIMAL(10, 2),
       allowNull: false,
-
+      isFloat: {
+        msg: "Value entered must be a number"
+      },
+      len: [1,10],
     },
     createdAt: {
       allowNull: false,
