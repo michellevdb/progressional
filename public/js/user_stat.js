6292a9318cf88e0268732c1a4d0ac457ede3748b
Ritchie Garcia <detectives.salvajes@protonmail.com>
2019-04-04 21:21:43 -0700

    graph works!

--- public/js/user_stat.js
+++ public/js/user_stat.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
-
   var selectedUser = $(".dropdown-menu").attr("data-id");
-
   //grabs users from database in prep for list creation
   $.get("/api/users", function (data) {
     console.log(data);
-
     if (data.length !== 0) {
-
       for (var i = 0; i < data.length; i++) {
         var row = $("<a>");
         row.addClass("dropdown-item");
@@ -15,64 +11,64 @@ $(document).ready(function () {
         row.attr("data-name", data[i].name);
         row.attr("data-id", data[i].id);
         row.text(data[i].name);
-
         $(".dropdown-menu").append(row);
       }
     }
   });
-
-    //grabs value from user select menu
-    $(".dropdown-menu").on("click", "a", function(event) {
-
-      event.preventDefault();
-
-      var selectedUser = $(this).text();
-      var selectedUserID = $(this).attr("data-id");
-
-      //assigns button text to name of selected user
-      $(".dropdown-toggle").text(selectedUser);
-      
-      //creates attritubes in html element to later be grabbed for ajax post
-      $(".dropdown-toggle").attr("data-name", selectedUser);
-      $(".dropdown-toggle").attr("data-id", selectedUserID);
-
-      console.log("User selected: " + selectedUser);
+  //grabs value from user select menu
+  $(".dropdown-menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var selectedUser = $(this).text();
+    var selectedUserID = $(this).attr("data-id");
+    //assigns button text to name of selected user
+    $(".dropdown-toggle").text(selectedUser);
+    //creates attritubes in html element to later be grabbed for ajax post
+    $(".dropdown-toggle").attr("data-name", selectedUser);
+    $(".dropdown-toggle").attr("data-id", selectedUserID);
+    console.log("User selected: " + selectedUser);
   });
-
   //grabs value of button clicked, hopefully generates a graph
-  $(".query-button").on("click", function() {
-
+  $(".query-button").on("click", function () {
     var buttonValue = $(this).attr("data-measurement");
     var selectedValue = buttonValue.toString();
     var selectedUser = $(".dropdown-toggle").attr("data-id");
-
     console.log("measurement selector clicked, button clicked: " + buttonValue);
     console.log("selected user is: " + selectedUser);
-
     $.get("/api/users/" + selectedUser)
-      .then(function(data) {
-      
-        var theData = data.Measurements;
+      .then(function (response) {
+        console.log(response);
+        var theData = response.Measurements;
         var labels = [];
         var data = [];
-
         console.log(buttonValue);
-
-          for (i=0; i < theData.length; i++) {
-
-            console.log(theData[i][selectedValue]);
-            data.push(theData[i][selectedValue]);
-
-            console.log(theData[i].createdAt.slice(5, 10));
-            labels.push(theData[i].createdAt.slice(5, 10));
-
-            console.log(labels);
-            console.log(data);
-
-          };
-        
+        for (i = 0; i < theData.length; i++) {
+          console.log(theData[i][selectedValue]);
+          data.push(theData[i][selectedValue]);
+          console.log(theData[i].createdAt.slice(5, 10));
+          labels.push(theData[i].createdAt.slice(5, 10));
+          console.log(labels);
+          console.log(data);
+        }
+        var aChart = $("#lineChart");
+        var lineChart = new Chart(aChart, {
+          type: 'line',
+          data: {
+            labels: labels,
+            datasets: [{
+              data: data,
+              label: selectedValue,
+              borderColor: "#3e95cd",
+              fill: false
+            }, 
+            ]
+          },
+          options: {
+            title: {
+              display: true,
+              text: 'M-body Fitness Tracker'
+            }
+          }
+        });
       });
   });
-
-
 });
